6d7c4e10d571bf366c305ff2a09190cb52c8a18b
Geovana <geovanacapizani@gmail.com>
2026-04-02 08:53:57 -0300

    Add files via upload

---
+++ New project/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1775130078,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite11-sheet0.png",
+		"images/sprite11-sheet1.png",
+		"images/sprite12-sheet0.png",
+		"images/sprite14-sheet0.png",
+		"images/sprite15-sheet0.png",
+		"images/sprite17-sheet0.png",
+		"images/sprite19-sheet0.png",
+		"images/sprite20-sheet0.png",
+		"images/sprite22-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
